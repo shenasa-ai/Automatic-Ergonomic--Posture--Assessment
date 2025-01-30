5554bd51a5cde2c1e1576a3bf0d9aa3af78b42b8
Ali Yazdanifar <yazdanifar1381@gmail.com>
2025-01-30 18:59:37 +0330

    add run.py to use utils function directly

--- utils/run.py
+++ utils/run.py
@@ -1,12 +1,18 @@
 from fleiss_kapa import FleissKappa
 from final_label_images import finalize_image_labels
 from clear_labels import clean_labels
+from Evaluation import calculate_accuracy
 
 '''
 1 - run finalize_image_labels
 2 - run clean_labels
 '''
 
-path = ['/home/ali/Desktop/python/posture/labels/ha_checked_side.csv', '/home/ali/Desktop/python/posture/labels/Labels 2 - side_fj.csv', '/home/ali/Desktop/python/posture/labels/Labels 2 - side_aa.csv']
+# path = ['./../labels/aa.csv', './../labels/fj.csv', './../labels/ha.csv']
+# image2remove = [123, 89, 96, 158, 159, 171, 172, 213, 232, 244, 245, 247, 248, 265, 284]
+#
+# finalize_image_labels(img_path='/home/ali/Desktop/Automatic-Ergonomic-Posture-Assessment/input/main_input/side', lbl_paths=path, save_path='./../labels', rem_img=True)
+# clean_labels(lbl_paths='./../labels/final_labels.csv',rem_img=image2remove , img_path='/home/ali/Desktop/Automatic-Ergonomic-Posture-Assessment/input/main_input/side', save_path='./../labels')
 
-clean_labels(images_dir='/home/ali/Desktop/python/posture/input/side', labels_path='./final_labels.csv')
+# Evaluate
+print(calculate_accuracy(pred_part='monitor', actual_part='monitor', pred_path='/home/ali/Desktop/Automatic-Ergonomic-Posture-Assessment/output/pred_Openpifpaf.csv', actual_path='/home/ali/Desktop/Automatic-Ergonomic-Posture-Assessment/labels/final_labels_cleared.csv'))
